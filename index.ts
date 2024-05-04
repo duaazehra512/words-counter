024af0aca5472bc2995c5f62658561ee218ea124
duaazehra512 <165053825+duaazehra512@users.noreply.github.com>
2024-05-04 23:54:29 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+
+import chalk from "chalk"
+import inquirer from "inquirer"
+console.log(chalk.yellow("Hello"))
+const answer:{
+    sentence:string
+} = await inquirer.prompt([
+    {
+name:"sentence",
+type:"input",
+message:"Enter your sentence to count the word"
+}
+
+])
+const words =answer.sentence.trim().split(" ")
+console.log(chalk.green(words))
+
+console.log(chalk.magentaBright(`your sentence word count is ${words.length}`));
+
+console.log(chalk.yellow("Thankyou"))
